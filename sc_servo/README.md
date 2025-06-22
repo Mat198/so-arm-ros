6d88c2e4244611576691dcc0795891362234f1cf
Matheus Sousa <matheussousasf@gmail.com>
2025-06-22 17:32:17 -0300

    SC Servo Drive package

---
+++ sc_servo/README.md
@@ -0,0 +1,5 @@
+# SC Servo Motors driver
+
+Driver package adaped from https://www.waveshare.com/wiki/Bus_Servo_Adapter_(A)
+
+Ideally shoud be a git submodule. Couldn't find one. **TODO:** Serch more later
---
+++ sc_servo/include/sc_servo/SCServo.hpp
@@ -0,0 +1,15 @@
+/*
+ * SCServo.h
+ * 串行舵机接口
+ * 日期: 2021.12.8
+ * 作者: 
+ */
+
+#ifndef _SCSERVO_H
+#define _SCSERVO_H
+
+#include "sc_servo/SMSBL.hpp" 
+#include "sc_servo/SCSCL.hpp"
+#include "sc_servo/SMSCL.hpp"
+#include "sc_servo/SMS_STS.hpp"
+#endif
