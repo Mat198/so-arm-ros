65d2e252a823cd8cec4efe78332d198c45649b87
Matheus Sousa <matheussousasf@gmail.com>
2025-06-28 20:22:09 -0300

    Structures to store arm data

---
+++ so_arm_driver/include/so_arm_driver/so_arm_data.hpp
@@ -0,0 +1,48 @@
+#ifndef SO_ARM_CONSTS_HPP
+#define SO_ARM_CONSTS_HPP
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace SOArm {
+
+static constexpr size_t JOINT_NUMBER = 6;
+using JointArray = std::array<double, JOINT_NUMBER>;
+using JointIntArray = std::array<double, JOINT_NUMBER>;
+
+struct Target {
+    JointArray pos;
+    JointArray vel;
+    JointArray acc;
+};
+
+struct State {
+    JointArray pos;
+    JointArray vel;
+    JointArray acc;
+    JointIntArray encoder;
+    JointIntArray load;
+    JointIntArray voltage;
+    JointIntArray temperature;
+};
+
+template<typename T>
+struct Limit {
+    T min;
+    T max;
+};
+
+struct JointLimits {
+    std::array<Limit<double>, JOINT_NUMBER> angle;
+    std::array<Limit<int>, JOINT_NUMBER> encoder;
+};
+
+
+// Convert consts in the format y = a*x + b
+struct ConvertConst {
+    double slope; //  constant
+    double intercept; // intercep
+};
+using JointConsts = std::array<ConvertConst, JOINT_NUMBER>;
+
+}  // namespace SOArm
+#endif  // SO_ARM_CONSTS_HPP
